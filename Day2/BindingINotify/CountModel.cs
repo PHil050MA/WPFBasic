712515bd7402ea9b0a43e621ea95f5da519c5e0a
YOSong1 <43607595+YOSong1@users.noreply.github.com>
2024-10-08 17:21:04 +0900

    Add files via upload

---
+++ Day2/BindingINotify/CountModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfDay2Binding
+{
+    internal class CountModel : INotifyPropertyChanged
+    {
+        private int counter;
+        public int Counter { 
+            get { return counter; } 
+            set { counter = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void OnPropertyChanged(string propertyName="")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
