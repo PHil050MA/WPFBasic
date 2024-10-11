933981a7f1386b2dd55c3c71c12f8a7f018c4f2a
YOSong1 <43607595+YOSong1@users.noreply.github.com>
2024-10-11 14:59:29 +0900

    Add files via upload

---
+++ Day4/JobManagerTimer/StopwatchControl.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace JobManagerTimer
+{
+    using JobManagerTimer.ViewModel;
+
+    /// <summary>
+    /// StopwatchControl.xaml에 대한 상호 작용 논리
+    /// </summary>    
+    using System.Windows.Threading;
+
+    /// <summary>
+    /// Interaction logic for StopwatchControl.xaml
+    /// </summary>
+    public partial class StopwatchControl : UserControl
+    {
+        DispatcherTimer _timer = new DispatcherTimer();
+        StopwatchViewModel _stopwatchViewModel;
+
+        public StopwatchControl()
+        {
+            InitializeComponent();
+
+            _stopwatchViewModel = Resources["viewModel"] as StopwatchViewModel;
+
+            _timer.Interval = TimeSpan.FromMilliseconds(100);
+            _timer.Tick += TimerTick;
+            _timer.Start();
+        }
+
+        private void TimerTick(object sender, EventArgs e)
+        {
+            _stopwatchViewModel.OnPropertyChanged(String.Empty);
+        }
+
+        private void StartStopButton_Click(object sender, RoutedEventArgs e)
+        {
+            _stopwatchViewModel.StartStop();
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            _stopwatchViewModel.Reset();
+        }
+
+        private void LapButton_Click(object sender, RoutedEventArgs e)
+        {
+            _stopwatchViewModel.LapTime();
+        }
+    }
+}
