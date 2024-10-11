8bacbcb496b8016c802a3e22f481914d3f2b99fb
YOSong1 <43607595+YOSong1@users.noreply.github.com>
2024-10-11 14:45:50 +0900

    Add files via upload

---
+++ Day4/TodoList/EditPopup.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using ToDoListApp;
+
+namespace ToDoListApp
+{
+    /// <summary>
+    /// EditPopup.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class EditPopup : Window
+    {
+        private string tempdesc;
+        public EditPopup(ToDoItem item)
+        {
+            InitializeComponent();
+            DataContext = item;
+            tempdesc = item.Description;
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            bool result = DialogResult ?? false;
+            if (!result)
+                (DataContext as ToDoItem).Description = tempdesc;
+        }
+
+        private void OK_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+            Close();
+        }
+    }
+}
---
+++ Day4/TodoList/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+
+namespace ToDoListApp
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ToDoList tdl = new ToDoList();
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = tdl;
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            if (!txtItemDesc.Text.Trim().Equals(string.Empty))
+            {
+                tdl.Additem(txtItemDesc.Text.Trim());
+            }
+            txtItemDesc.Text = "";
+            lvToDo.Items.Refresh();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            tdl.Save();
+        }
+
+        private void MarkAsDone(object sender, RoutedEventArgs e)
+        {
+            if (lvToDo.SelectedItem != null)
+            {
+                MessageBoxResult done = MessageBox.Show("Mark this as done?", "Done?", MessageBoxButton.YesNo, MessageBoxImage.Question, MessageBoxResult.No);
+                if (done == MessageBoxResult.Yes)
+                {
+                    (lvToDo.SelectedItem as ToDoItem).DoneDateTime = DateTime.Now.ToString("yyyy-MM-ddThh:mm:ss.ms");
+                }
+                lvToDo.Items.Refresh();
+            }
+        }
+
+        private void lvToDo_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            MarkAsDone(sender, e);
+        }
+
+        private void EditMenu_Click(object sender, RoutedEventArgs e)
+        {
+            if (lvToDo.SelectedItem != null)
+            {
+                EditPopup ep = new EditPopup(lvToDo.SelectedItem as ToDoItem);
+                ep.Owner = this;
+                ep.ShowDialog();
+            }
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            if (lvToDo.SelectedItem != null)
+            {
+                MessageBoxResult del = MessageBox.Show("Delete this item?", "Delete?", MessageBoxButton.YesNo, MessageBoxImage.Question, MessageBoxResult.No);
+                if (del == MessageBoxResult.Yes)
+                {
+                    tdl.DeleteItem(lvToDo.SelectedItem as ToDoItem);
+                }
+                lvToDo.Items.Refresh();
+            }
+        }
+    }
+}
